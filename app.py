62ba12acfa010e7c6e62c3730380c221474fe02a
Marcelo <mlnunes@hotmail.com>
2019-12-31 11:35:18 -0200

    Fase2

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 import pyautogui
-import os
+import pandas as pd
 
-print (os.getcwd())
+
+documento = "0181900"
 im = pyautogui.screenshot()
-arquivo = "print_screen.jpg"
-im.save(arquivo)
+posicao = pyautogui.locateOnScreen('.\\src\\caixa_pesquisa.png')
+caixa_pesquisa = [(posicao[0]+int(posicao[2]/2)), (posicao[1]+int(posicao[3]/2))]
+pyautogui.click(caixa_pesquisa, button='left')
+pyautogui.typewrite(documento)
+pyautogui.typewrite('\n')
+pyautogui.PAUSE = 0.5
+posicao = None
+while posicao == None:
+    posicao = pyautogui.locateOnScreen('.\\src\\botao_correio.png')
+    
+botao_correio = [(posicao[0]+int(posicao[2]/2)), (posicao[1]+int(posicao[3]/2))]
+pyautogui.click(botao_correio, button='left')
+
+posicao = None
+while posicao == None:
+    posicao = pyautogui.locateOnScreen('.\\src\\tipo_impressao.png')
+
+selecao_impressao = [(posicao[0]+10), (posicao[1]+10)]
+pyautogui.click(selecao_impressao, button='left')
+posicao = pyautogui.locateOnScreen('.\\src\\solicitar_expedicao.png')
+botao_expedicao = [(posicao[0]+int(posicao[2]/2)), (posicao[1]+int(posicao[3]/2))]
+pyautogui.click(botao_expedicao, button='left')
+
